ca29f129d8f94a0d63b7409f97e5019d8ae6c7f4
prisilamichelle <prisilamichelle@yahoo.com>
2019-05-03 11:14:12 +0700

    Updates

---
+++ precache-manifest.aac8cc9a3421bb9f0a2a1db1a1f2a953.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92696c8f4931833666249c18a692ab29",
+    "url": "/game-acak-kata/index.html"
+  },
+  {
+    "revision": "f9067ad62143f8f4a009",
+    "url": "/game-acak-kata/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/game-acak-kata/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "f9067ad62143f8f4a009",
+    "url": "/game-acak-kata/static/js/main.e57a1508.chunk.js"
+  },
+  {
+    "revision": "23473c1fa05a2093f3ab",
+    "url": "/game-acak-kata/static/js/runtime~main.1dd33dfd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/game-acak-kata/static/media/logo.5d5d9eef.svg"
+  }
+]);
